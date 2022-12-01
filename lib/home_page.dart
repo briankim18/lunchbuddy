f04a164c145d8c297c1a7854a52334b7e8968ba2
Brian Kim <briankim18@vt.edu>
2022-11-30 19:28:20 -0500

    ui for message

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,7 @@ import 'package:lunch_buddy/authentication_service.dart';
 import 'package:flutter/material.dart';
 import 'package:lunch_buddy/main.dart';
 import 'package:lunch_buddy/messaging_page.dart';
+import 'package:lunch_buddy/page/messages_page.dart';
 import 'package:lunch_buddy/public_request_page.dart';
 import 'package:lunch_buddy/settings_page.dart';
 import 'package:lunch_buddy/user_profile_page.dart';
@@ -18,7 +19,7 @@ class _HomePageState extends State<HomePage> {
   int currentPage = 0;
   List<Widget> pages = const [
     PublicRequestPage(),
-    MessagingPage(),
+    MessagesPage(),
     UserProfilePage(),
     SettingsPage(),
   ];
