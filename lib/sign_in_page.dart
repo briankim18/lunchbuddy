378a3f162f93eba9dcb40922a1158c35b8ffbfb4
marktd19 <marktd19@vt.edu>
2022-12-02 16:42:30 -0500

    whatever

--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -1,6 +1,7 @@
 import 'package:lunch_buddy/authentication_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:lunch_buddy/home_page.dart';
 
 class SignInPage extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
@@ -15,17 +16,46 @@ class SignInPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  contentPadding: const EdgeInsets.all(12), isDense: true,
-                  labelText: "Email",
+              ButtonBar(children: [
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<AuthenticationService>().signIn(
+                      email: emailController.text.trim(),
+                      password: passwordController.text.trim(),
+                    );
+                  },
+                  child: const Text("Sign in"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomePage()),
+                    );
+                  },
+                  child: const Text("Register"),
+                ),
+              ]),
+              const Text(
+                "Lunch Buddy",
+                style: TextStyle(
+                    fontFamily: 'Times New Roman', 
+                    fontSize: 45, 
+                    color: Colors.black)),
+              Padding(padding: EdgeInsets.all(20.0),
+                child: TextField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    labelText: "Email",
+                  ),
                 ),
               ),
-              TextField(
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  labelText: "Password",
+              Padding(padding: EdgeInsets.all(20.0),
+                child: TextField(
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                    labelText: "Password",
+                  ),
                 ),
               ),
               ElevatedButton(
@@ -35,8 +65,14 @@ class SignInPage extends StatelessWidget {
                     password: passwordController.text.trim(),
                   );
                 },
-                child: const Text("Sign in"),
-              )
+                child: const Text("Login"),
+              ),
+              ClipRRect(
+                  child: Image.asset(
+                      'images/BrownBag.png',
+                      width: 150,
+                      height: 150)
+              ),
             ],
           ),
         )
