13a3ee01916a583fcbd676ceed2d8d783943fcc7
marktd19 <marktd19@vt.edu>
2022-12-02 19:47:52 -0500

    updated Register/Login page

--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -2,6 +2,8 @@ import 'package:lunch_buddy/authentication_service.dart';
 import 'package:flutter/material.dart';
 import 'package:lunch_buddy/sign_up_page.dart';
 import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 class SignInPage extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
@@ -12,43 +14,81 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 15, 255, 151),
         body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            controller: emailController,
-            decoration: const InputDecoration(
-              contentPadding: EdgeInsets.all(12),
-              isDense: true,
-              labelText: "Email",
-            ),
-          ),
-          TextField(
-            controller: passwordController,
-            decoration: const InputDecoration(
-              labelText: "Password",
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              context.read<AuthenticationService>().signIn(
-                    email: emailController.text.trim(),
-                    password: passwordController.text.trim(),
-                  );
-            },
-            child: const Text("Sign In"),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const SignUpPage())
-              );
-            }, child: const Text("Register")
-          )
-        ],
-      ),
-    ));
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(20, 40, 20, 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Lunch Buddy",
+                      style: GoogleFonts.indieFlower(fontSize: 50)),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                        "Username", style: GoogleFonts.indieFlower(
+                        fontSize: 20, height: 2)),
+                  ),
+                  TextField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.all(12),
+                      isDense: true,
+                      labelText: "Username",
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                        "Password", style: GoogleFonts.indieFlower(
+                        fontSize: 20, height: 2)),
+                  ),
+                  TextField(
+                    controller: passwordController,
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.all(12),
+                      labelText: "Password",
+                      isDense: true,
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      context.read<AuthenticationService>().signIn(
+                        email: emailController.text.trim(),
+                        password: passwordController.text.trim(),
+                      );
+                    },
+                    child: const Text("Sign In"),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SignUpPage())
+                        );
+                      }, child: const Text("Register")
+                  ),
+                  SizedBox(height: 50),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Image.asset(
+                        'images/BrownBag.png',
+                        height: 150,
+                        width: 150,
+                      )
+                  ),
+                ],
+              ),
+            )
+        )
+    );
   }
 }
