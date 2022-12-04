5db752602caac30fb5a2a7caf281b652c4650b28
marktd19 <marktd19@vt.edu>
2022-12-04 15:12:27 -0500

    sign in scrolling

--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -14,81 +14,84 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 15, 255, 151),
-        body: Center(
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(20, 40, 20, 0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Lunch Buddy",
-                      style: GoogleFonts.indieFlower(fontSize: 50)),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                        "Username", style: GoogleFonts.indieFlower(
-                        fontSize: 20, height: 2)),
+      backgroundColor: const Color.fromARGB(255, 15, 255, 151),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(20, 40, 20, 0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Lunch Buddy",
+                    style: GoogleFonts.indieFlower(fontSize: 50)),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                      "Username", style: GoogleFonts.indieFlower(
+                      fontSize: 20, height: 2)),
+                ),
+                TextField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.all(12),
+                    isDense: true,
+                    labelText: "Username",
+                    border: OutlineInputBorder(),
+                    filled: true,
+                    fillColor: Colors.white,
                   ),
-                  TextField(
-                    controller: emailController,
-                    decoration: const InputDecoration(
-                      contentPadding: EdgeInsets.all(12),
-                      isDense: true,
-                      labelText: "Username",
-                      border: OutlineInputBorder(),
-                      filled: true,
-                      fillColor: Colors.white,
-                    ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                      "Password", style: GoogleFonts.indieFlower(
+                      fontSize: 20, height: 2)),
+                ),
+                TextField(
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.all(12),
+                    labelText: "Password",
+                    isDense: true,
+                    border: OutlineInputBorder(),
+                    filled: true,
+                    fillColor: Colors.white,
                   ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                        "Password", style: GoogleFonts.indieFlower(
-                        fontSize: 20, height: 2)),
-                  ),
-                  TextField(
-                    controller: passwordController,
-                    decoration: const InputDecoration(
-                      contentPadding: EdgeInsets.all(12),
-                      labelText: "Password",
-                      isDense: true,
-                      border: OutlineInputBorder(),
-                      filled: true,
-                      fillColor: Colors.white,
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<AuthenticationService>().signIn(
+                      email: emailController.text.trim(),
+                      password: passwordController.text.trim(),
+                    );
+                  },
+                  child: const Text("Sign In"),
+                ),
+                ElevatedButton(
                     onPressed: () {
-                      context.read<AuthenticationService>().signIn(
-                        email: emailController.text.trim(),
-                        password: passwordController.text.trim(),
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignUpPage())
                       );
-                    },
-                    child: const Text("Sign In"),
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const SignUpPage())
-                        );
-                      }, child: const Text("Register")
-                  ),
-                  SizedBox(height: 50),
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: Image.asset(
-                        'images/BrownBag.png',
-                        height: 150,
-                        width: 150,
-                      )
-                  ),
-                ],
-              ),
-            )
-        )
+                    }, child: const Text("Register")
+                ),
+                SizedBox(height: 50),
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: Image.asset(
+                      'images/BrownBag.png',
+                      height: 150,
+                      width: 150,
+                    )
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
