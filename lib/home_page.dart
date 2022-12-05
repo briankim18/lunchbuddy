70acc974e0f9895e816bc1be777d5cb154be033b
Neosdragon0 <popcornks@gmail.com>
2022-12-05 00:14:38 -0500

    fixed location spam

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:lunch_buddy/location_screen.dart';
 import 'package:lunch_buddy/main.dart';
+import 'package:lunch_buddy/messaging_page.dart';
 import 'package:lunch_buddy/new_public_request.dart';
-import 'package:lunch_buddy/page/messages_page.dart';
 
 import 'package:lunch_buddy/public_request_page.dart';
 import 'package:lunch_buddy/settings_page.dart';
@@ -19,11 +19,9 @@ class _HomePageState extends State<HomePage> {
   int currentPage = 0;
   List<Widget> pages = const [
     PublicRequestPage(),
-  //  MessagingPage(),
-    //MapSampleState(),
-    LocationScreen(),
-    MessagesPage(),
-  //  MessagingPage(),
+    // MapSampleState(),
+    // LocationScreen(),
+    MessagingPage(),
     HomePage(),
     UserProfilePage(),
     SettingsPage(),
